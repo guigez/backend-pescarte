d9ee3643e2cfd797e4a9f75fa236d6f3af94b818
Alex Junior <alexmomijunior@gmail.com>
2024-02-01 21:46:26 -0300

    Adding requirements.txt and creating first route

---
+++ src/app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI(title='Catalogo Pescarte API', version='0.0.1')
+
+
+@app.get('/healthcheck')
+async def health_check():
+    return {"message": "Up and running"}
